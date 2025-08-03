6557b7e2279f63a10868a2060be7b006419b3441
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-08-03 21:12:11 +0200

    Get existing github role from infra

--- infra/modules/iam/github/main.tf
+++ infra/modules/iam/github/main.tf
@@ -1,7 +1,5 @@
-resource "aws_iam_openid_connect_provider" "github" {
+data "aws_iam_openid_connect_provider" "github" {
   url            = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list= ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 resource "aws_iam_role" "github_actions" {
@@ -12,7 +10,7 @@ resource "aws_iam_role" "github_actions" {
     Statement = [{
       Effect    = "Allow",
       Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
+        Federated = data.aws_iam_openid_connect_provider.github.arn
       },
       Action    = "sts:AssumeRoleWithWebIdentity",
       Condition = {
